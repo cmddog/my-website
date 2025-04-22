2985887ff878ce30717ef89eae482281e8ce8f9d
Lorenz Balzereit <lorenz.balzereit@adesso.de>
2025-04-22 21:59:32 +0200

    i forgor to commit this but it has like everything and also prettier

---
+++ frontend/src/app/commission-page/commission-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-commission-page',
+  imports: [],
+  templateUrl: './commission-page.component.html',
+  styleUrl: './commission-page.component.css'
+})
+export class CommissionPageComponent {
+
+}
--- frontend/src/app/main-page/main-page.component.ts
+++ frontend/src/app/main-page/main-page.component.ts
@@ -1,14 +1,62 @@
-import { Component } from '@angular/core';
-import {NgOptimizedImage} from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { NgClass, NgOptimizedImage } from '@angular/common';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-main-page',
-  imports: [
-    NgOptimizedImage
-  ],
+  imports: [NgOptimizedImage, NgClass],
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.css'
+  styleUrl: './main-page.component.css',
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
+  spriteNumber = 0;
+  previousSprite = 0;
+  isChanging = true;
+  initialAnimation = true;
+  spriteLoadedArray: boolean[] = [false, false, false];
 
+  ngOnInit(): void {
+    console.log('Initial values', this.spriteLoadedArray);
+    if (localStorage.getItem('alreadyVisited')) {
+      this.spriteNumber = this.randomSprite();
+    } else {
+      this.spriteNumber = 1;
+      localStorage.setItem('alreadyVisited', 'true');
+    }
+    console.log('sprite number initiated: ', this.spriteNumber);
+    setTimeout(() => {
+      this.initialAnimation = false;
+    }, 200);
+  }
+
+  randomSprite(n?: number) {
+    if (!n) this.spriteNumber = 0;
+    do {
+      n = Math.floor(Math.random() * 3) + 1;
+    } while (n === this.previousSprite);
+    return n;
+  }
+
+  changeSprite() {
+    if (this.isChanging) return;
+    this.previousSprite = this.spriteNumber;
+    this.spriteNumber = 0;
+    this.isChanging = true;
+    setTimeout(() => {
+      this.spriteNumber = this.randomSprite(this.previousSprite);
+      setTimeout(() => {
+        this.isChanging = false;
+      }, 150);
+    }, 200);
+  }
+
+  spriteLoaded(n: number) {
+    this.spriteLoadedArray[n - 1] = true;
+    if (n === this.spriteNumber) {
+      setTimeout(() => {
+        this.isChanging = false;
+      }, 150);
+    }
+    console.log('Adjusted load array:', this.spriteLoadedArray);
+  }
 }
