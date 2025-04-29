dc845f2e2aaf5fa78bbc87f1229bd745244e4959
cmddog <cmddog@users.noreply.github.com>
2025-04-30 01:20:09 +0200

    stuff and such

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
+import { CommissionPageComponent } from './commission-page/commission-page.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainPageComponent,
+  },
+  {
+    path: 'commissions',
+    component: CommissionPageComponent,
   },
 ];
