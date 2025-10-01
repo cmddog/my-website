7b9ca122be5e7dda43b956c2d1e41d6974646514
cmddog <cmddog@users.noreply.github.com>
2025-10-01 23:58:28 +0200

    add option to hide skeleton loader and add debounce time

--- frontend/src/app/skeleton-loader/skeleton-loader.component.ts
+++ frontend/src/app/skeleton-loader/skeleton-loader.component.ts
@@ -1,5 +1,6 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-skeleton-loader',
@@ -8,8 +9,27 @@ import { CommonModule } from '@angular/common';
   templateUrl: "skeleton-loader.component.html",
   styleUrl: "skeleton-loader.component.scss"
 })
-export class SkeletonLoaderComponent {
+export class SkeletonLoaderComponent implements OnInit, OnDestroy {
   width = input<string>('100%');
   height = input<string>('100%');
   borderRadius = input<string>('2rem');
+  display = input<boolean>(true);
+  debounceTime = input<number>(100);
+
+  showLoader = signal(false);
+  private timeout?: number;
+
+  ngOnInit() {
+    if (this.display()) {
+      this.timeout = window.setTimeout(() => {
+        this.showLoader.set(true);
+      }, this.debounceTime());
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+    }
+  }
 }
