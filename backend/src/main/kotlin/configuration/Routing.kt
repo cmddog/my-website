9ad590e87f807b204de63c02952154af208195f1
cmddog <cmddog@users.noreply.github.com>
2026-01-21 19:46:24 +0100

    implement admin login backend

--- backend/src/main/kotlin/configuration/Routing.kt
+++ backend/src/main/kotlin/configuration/Routing.kt
@@ -1,7 +1,10 @@
 package com.cmddog.configuration
 
 import com.cmddog.models.Commission
+import com.cmddog.models.ErrorResponse
+import com.cmddog.models.LoginRequest
 import com.cmddog.services.PublicInfoService
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.ratelimit.*
@@ -9,7 +12,12 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.sessions.*
+import org.ktorm.logging.Logger
+import org.mindrot.jbcrypt.BCrypt
+import org.slf4j.LoggerFactory
 import java.util.*
+
+private val logger = KotlinLogging.logger {  }
 
 fun Application.configureRouting() {
     routing {
@@ -24,9 +32,24 @@ fun Application.configureRouting() {
 
                 // Admin login
                 post("/admin/login") {
-                    val session = AdminSession(sessionId = UUID.randomUUID().toString())
+                    val pass = call.receive<LoginRequest>().password
+                    val adminPassHash = System.getenv("ADMIN_PASS_HASH")
+
+                    if (adminPassHash == null) {
+                        call.respond(HttpStatusCode.InternalServerError)
+                        logger.error { "Missing ADMIN_PASS_HASH environment variable." }
+                        return@post
+                    }
+
+                    if (!BCrypt.checkpw(pass, adminPassHash)) {
+                        call.respond(HttpStatusCode.Unauthorized, ErrorResponse("Incorrect Password"))
+                        return@post
+                    }
+
+                    val session = AdminSession(UUID.randomUUID().toString())
+
                     call.sessions.set(session)
-                    call.respond(HttpStatusCode.OK)
+                    call.respond(HttpStatusCode.OK, session.sessionId)
                 }
             }
 
