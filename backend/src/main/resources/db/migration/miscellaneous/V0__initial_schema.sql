66dc31691f975eb86a6f3c3524803fb577f21145
cmddog <cmddog@users.noreply.github.com>
2026-03-05 22:16:45 +0100

    add bio and pfp to users

--- backend/src/main/resources/db/migration/miscellaneous/V0__initial_schema.sql
+++ backend/src/main/resources/db/migration/miscellaneous/V0__initial_schema.sql
@@ -1,11 +1,13 @@
 CREATE TABLE users
 (
-    username      VARCHAR(32) PRIMARY KEY, -- must be lowercase
-    display_name  VARCHAR(32),             -- must be identical to username except for capitalization
-    password_hash VARCHAR(128),
-    messages_sent INT,
-    created       DATE DEFAULT now(),
-    last_seen     DATE DEFAULT now()
+    username        VARCHAR(32) PRIMARY KEY, -- must be lowercase
+    display_name    VARCHAR(32),             -- must be identical to username except for capitalization
+    password_hash   VARCHAR(128) NOT NULL,
+    bio             VARCHAR(500),
+    profile_picture VARCHAR(512),
+    messages_sent   INT,
+    created         DATE DEFAULT now(),
+    last_seen       DATE DEFAULT now()
 );
 
 CREATE OR REPLACE FUNCTION validate_user()
@@ -28,11 +30,12 @@ CREATE OR REPLACE TRIGGER validate_user_trigger
     FOR EACH ROW
 EXECUTE FUNCTION validate_user();
 
-INSERT INTO users VALUES (
-                          'shiru',
-                          'Shiru',
-                          '$2a$12$PTQS3qkVQPTWKeu22/VUZedli8wMcL3w1IZ3eGMkJYuWdkGvv9Dkm',
-                          0,
-                          to_timestamp('01/01/0000', 'DD/MM/YYYY'),
-                          DEFAULT
-                         );
+INSERT INTO users
+VALUES ('shiru',
+        'Shiru',
+        '$2a$12$PTQS3qkVQPTWKeu22/VUZedli8wMcL3w1IZ3eGMkJYuWdkGvv9Dkm',
+        NULL,
+        NULL,
+        0,
+        to_timestamp('01/01/0000', 'DD/MM/YYYY'),
+        DEFAULT);
