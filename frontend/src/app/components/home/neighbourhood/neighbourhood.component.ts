1adda6ed16e93ba684be3ddc8c5a9a5c183bf402
cmddog <cmddog@users.noreply.github.com>
2026-04-27 23:04:29 +0200

    also click to copy

--- frontend/src/app/components/home/neighbourhood/neighbourhood.component.ts
+++ frontend/src/app/components/home/neighbourhood/neighbourhood.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-neighbourhood',
@@ -6,4 +6,22 @@ import { Component } from '@angular/core';
   templateUrl: './neighbourhood.component.html',
   styleUrl: './neighbourhood.component.scss',
 })
-export class NeighbourhoodComponent {}
+export class NeighbourhoodComponent {
+  copied = signal(false);
+
+  readonly code = `<a href="https://shiru.dog" target="_blank" rel="noopener noreferrer">
+  <img
+    src="https://shiru.dog/assets/button.png"
+    alt="Shiru's site"
+    height="31"
+    width="88"
+  />
+</a>`;
+
+  copy() {
+    navigator.clipboard.writeText(this.code).then(() => {
+      this.copied.set(true);
+      setTimeout(() => this.copied.set(false), 3000);
+    });
+  }
+}
