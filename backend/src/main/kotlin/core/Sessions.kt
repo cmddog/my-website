be07410e4a6bf000b8fa67fc23ea9207f946048f
cmddog <cmddog@users.noreply.github.com>
2026-04-27 15:49:27 +0200

    waow security

--- backend/src/main/kotlin/core/Sessions.kt
+++ backend/src/main/kotlin/core/Sessions.kt
@@ -18,14 +18,20 @@ fun Application.configureSessions() {
         cookie<AdminSession>("ADMIN_SESSION") {
             cookie.path = "/"
             cookie.maxAgeInSeconds = 60 * 60 * 24 // 1 day
+            cookie.httpOnly = true
+            cookie.secure = true
         }
         cookie<UserSession>("USER_SESSION") {
             cookie.path = "/"
             cookie.maxAgeInSeconds = 60 * 60 * 24 * 7 // 7 days
+            cookie.httpOnly = true
+            cookie.secure = true
         }
         cookie<GuestSession>("GUEST_SESSION") {
             cookie.path = "/"
             cookie.maxAgeInSeconds = 60 * 60 * 8 // 8 hours
+            cookie.httpOnly = true
+            cookie.secure = true
         }
     }
 }
