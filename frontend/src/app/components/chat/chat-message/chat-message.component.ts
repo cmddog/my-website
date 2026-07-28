ee0ffb2b618b506174f75fa8d12eaed47510bc92
cmddog <cmddog@users.noreply.github.com>
2026-07-28 16:14:58 +0200

    fix month, changelog

--- frontend/src/app/components/chat/chat-message/chat-message.component.ts
+++ frontend/src/app/components/chat/chat-message/chat-message.component.ts
@@ -11,6 +11,6 @@ export class ChatMessageComponent {
   readonly message = input.required<DisplayMessage>();
   time = computed(() => {
     const t = new Date(this.message().timestamp);
-    return `${t.getFullYear()}/${t.getMonth()}/${t.getDate()} ${t.getHours()}:${t.getMinutes().toString().length === 1 ? '0' : ''}${t.getMinutes()}`;
+    return `${t.getFullYear()}/${t.getMonth() + 1}/${t.getDate()} ${t.getHours()}:${t.getMinutes().toString().length === 1 ? '0' : ''}${t.getMinutes()}`;
   });
 }
